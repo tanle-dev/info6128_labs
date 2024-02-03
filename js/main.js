7e13bb1e3722b38e53a329a3cda5b06b4c6dd048
tanle <lenhuttan.programing@gmail.com>
2024-02-02 22:33:41 -0500

    first-commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// Author: Tan Le
+// Last modified: 2024/02/02
+
+
+/**
+ * 
+ * @param elementId - id of element to validate
+ * @returns  true if the value of element is not empty, false otherwise
+ */
+function validateForm(elementId){
+    let element = document.getElementById(elementId);
+    if(element.value == ""){
+        element.nextElementSibling.style.display = "block";
+        return false
+    }
+    else{
+        element.nextElementSibling.style.display = "none";
+    }
+    return true
+}
+
+/**
+ *  Add new song to the list
+ * @returns  none
+ */
+function addSongToList() {
+    let songList = document.getElementsByClassName("song__list")[0];
+    let songName = document.getElementById("song__title");
+    let artistName = document.getElementById("song__artist");
+
+    let songItem = document.getElementsByClassName("song__item")[0].cloneNode(true);
+    songItem.removeAttribute("hidden");
+    songList.appendChild(songItem);
+    songItem.getElementsByClassName("song__name")[0].innerHTML = songName.value;
+    songName.value = "";
+    songItem.getElementsByClassName("singer")[0].innerHTML = artistName.value;
+    artistName.value = "";
+}
+
+/**
+ * Add new song to the list when click submit button
+ * @returns  none
+ */
+function addNewSong() {
+    document.getElementById("submit__btn").addEventListener("click", () => {
+        validateForm("song__title");
+        validateForm("song__artist");
+        if(validateForm("song__title") && validateForm("song__artist"))
+        addSongToList();
+    });
+}
+
+/**
+ * Register service worker
+ * 
+ */
+if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker
+        .register('/service-worker.js', { scope: '/' })
+        .then( (registration)  => {
+            console;log('Service Worker is registered', registration);
+        })
+        .catch( (err) =>  {});
+}
---
+++ service-worker.js
@@ -0,0 +1,79 @@
+// Author: Tan Le
+// Last modified: 2024/02/02
+
+const cacheName = 'Cache Data V1.0';
+/**
+ * On Install Event.
+ * Triggered when the service worker is installed.
+ */
+self.addEventListener('install', function(event) {
+    // Activate itself when it enters the waiting phase.
+    // self.skipWaiting();
+
+    // Add a call to skipWaiting here
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function(cache) {
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/main.css',
+                    '/js/main.js',
+                    '/manifest.json',
+                    '/service-worker.js',
+                    '/assets/icons/android-chrome-144x144.png',
+                    '/assets/imgs/logo__192x192.png'
+                ])
+            })
+            .catch((error) => {})
+    )
+});
+
+/**
+ * On Activate Event.
+ * Triggered when the service worker is activated.
+ */
+self.addEventListener('activate', function(event) {
+    // Immediately get control over the open pages.
+    event.waitUntil(self.clients.claim());
+
+    // Remove old caches
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames)=> {
+                return Promise.all(
+                    cacheNames.filter(item => item != cacheName)
+                        .map(item => caches.delete(item))
+                )
+            })
+        )
+});
+
+
+/**
+ * On Fetch Event.
+ * Triggered when the service worker is fetching.
+ */
+self.addEventListener('fetch', function(event) {
+    // Stale While Revalidate
+    event.respondWith(
+        caches.open(cacheName)
+            .then((cache)=>{
+                return cache.match(event.request)
+                            .then((cachedRespond) => {
+                                const fetchedRespond = fetch(event.request)
+                                    .then((networkRespond) => {
+                                        cache.put(event.request, networkRespond.clone())
+                                        return networkRespond
+                                    })
+                                    .catch(() => {
+                                            return cache.match("/index.html")
+                                        }
+                                    )
+                                return cachedRespond || fetchedRespond
+                            })
+            })
+    )
+});
+
+
